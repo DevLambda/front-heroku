baf6c1d6cbfe81951faef360a026d630e5860b73
Marcela Rojas <marcelarojas_d@outlook.com>
2021-10-06 19:34:46 -0500

    Se arregla css de interfaz login

--- src/pages/login.jsx
+++ src/pages/login.jsx
@@ -7,26 +7,26 @@ function Login() {
             <div className="divlogo"> 
                <div className="divinternologo"><h1>DevLambda</h1></div>            
                 <div className="divinternopiedepagina">
-                <p >Equipo Devlambda/Grupo8/Ciclo3/UdeA/MisionTic 2022</p>
+                <p >Equipo DevLambda/Grupo8/Ciclo3/UdeA/MisionTic 2022</p>
                 </div>
             </div>  
 
             <div className="divlogin">
-                <h1>Bienvenido <br></br> a tu plataforma de ventas</h1>
-                <p>Ingresa tus datos.</p>
+                <h1>Bienvenido<br></br>a tu plataforma de ventas</h1>
+                <p>Ingresa tus datos.</p><br></br>
                 <form>
-                <label for="username"> Usuario</label><br />
-                <input type="text" className="label" placeholder="Enter Username"></input><br />
+                <label for="username"> Usuario</label><br/>
+                <input type="text"  placeholder="Enter Username"></input><br />
                 <label for="password">Contraseña</label><br />
-                <input type="password" className="label" placeholder="Enter Password"></input><br />
+                <input type="password" placeholder="Enter Password"></input><br />
                 <Link to='/Ventas'>
                 <input className="botonlogin" type="submit" value="Ingresar"></input><br />
                 </Link>
                 <script src="https://accounts.google.com/gsi/client" async defer></script>
-                </form>  
-                
+                </form>              
                 <br></br>
-                <span className="auth">Olvidó su contraseña?</span><span class="auth">Registrarme</span>
+                <span className="auth">¿No tienes cuenta?</span><span class="register">Regístrate</span>
+                {/* <span className="auth">Olvidé mi contraseña</span> */}
                 <div id="g_id_onload"
                   data-client_id="1062176196992-1b403cldkrck8tg2vqs57htuc4eptlrb.apps.googleusercontent.com"
                   data-ux_mode="redirect"
