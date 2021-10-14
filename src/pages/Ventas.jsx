3c53e1869515c3abbabe90553cc63b53ef32f143
Marcela Rojas <marcelarojas_d@outlook.com>
2021-10-13 19:37:52 -0500

    Se termina de ajustar css y html en react de registrar ventas

--- src/pages/Ventas.jsx
+++ src/pages/Ventas.jsx
@@ -194,12 +194,13 @@ const RegistrarVentas = ({ setMostrarTablaVentas, listaVentas, setVentas }) => {
             <section className="contenedorFormVentas"id="container">
                 <div className="datos_cliente">
                   <div className="action_cliente">
-                      <h4>Datos del cliente</h4>
-                      <a href="#" className="btn_new btn_new_cliente"><i className="fas fa-plus"></i>Nuevo Cliente</a>
+                      <h4>Datos del Cliente</h4>
+                      <a href="#" className="btn_new btn_new_cliente"><i class="fas fa-plus"></i>Nuevo Cliente</a>
                   </div>
                   <form ref={form} onSubmit={submitForm} name ="form_new_cliente_venta" id= "form_new_cliente_venta" class="datos">
                       <input id="input_ventas" type="hidden" name="action" value="addCliente"/>
                       <input id="input_ventas" type="hidden" name="idCliente" value="" required/>
+
                       <div className="wd20">
                           <label id="label">Cédula</label>
                           <input id="input_ventas" type="text" name="ced_cliente" required/>
@@ -237,76 +238,92 @@ const RegistrarVentas = ({ setMostrarTablaVentas, listaVentas, setVentas }) => {
                           <label id="label">Fecha de facturación</label>
                           <input id="input_fecha" type="date" name="fecha_fact" required/>
                       </div>
+                      <div className="wd100">
+                      </div>
+                  </div>
+              </div>
+
+              <div className="datos_venta">
+                  <h4>Datos Venta</h4>
+                  <div className="datos">
+                      {/* <div className="wd50">
+                          <label id="label">Código</label>
+                          <input id="input_ventas" type="text" name="code" required/>
+                      </div> */}
+                      <div className="wd50">
+                          <label id="label">Producto
+                          <select id="listaProductos" name="descripcion" required defaultValue={0} >
+                            <option disabled value={0}> Selecciona un producto</option>
+                                <option>Bonsai Chumono</option>
+                                <option>Bonsai Komono</option>
+                                <option>Bonsai Kotate</option>
+                                <option>Bonsai Omono</option>
+                                <option>Bonsai Shito</option>
+                                <option>Bonsai Shohin</option>
+                        </select>
+                      </label>
+                      </div>
+                      <div className="wd50">
+                          <label id="label">Cantidad</label>
+                          <input id="input_fecha" min={1} type="number" name="quantity" required/>
+                      </div>
+                      <div className="wd50">
+                          <label id="label">Precio</label>
+                          <input id="input_fecha" type="number" name="price" required disabled/>
+                      </div>
                       <div className="wd50">
                           <label id="label">Fecha de Pago</label>
                           <input id="input_fecha" type="date" name="fecha_pago" required/>
-                      </div>
-                      <div className="wd100">
-                          <label id="label">Acciones</label>
-                          <a href="#" className="btn_ok" id="btn_anular_venta"><i className="fas fa-ban"></i>Anular</a>
-                          <a href="#" className="btn_new" id="btn_facturar_venta"><i className="fas fa-edit"></i>Procesar</a>
                       </div>
                   </div>
               </div>
 
               <table border="1"id="tbl_venta">
                   <thead>
-                      {/* <tr>
-                          <th width="100px">Código</th>
-                          <th width="150px">Descripción</th>
-                          <th width="150px">Existencia</th>
-                          <th width="100px">Cantidad</th>
-                          <th class="150px">Precio</th>
-                          <th class="150px"> Acción</th>
-                      </tr> */}
-{/* 
                       <tr>
-                          <td><input id="input_ventas" type="text" name="txt_cod_producto"/></td>
-                          <td id="txt_descripcion">-</td>
-                          <td id="txt_existencia">-</td>
-                          <td><input id="input_ventas" type="text" name="txt_cant_producto" value="0" min="1" disabled/></td>
-                          <td id="txt_precio" className="textright">0.00</td>
-                          <td><a href="#" className="link_add" id="add_product_venta"><i className="fas fa-plus"></i></a></td>
-                      </tr> */}
-                      <tr>
-                          <th>Código</th>
-                          <th colspan="2">Descripción</th>
-                          <th>Cantidad</th>
-                          <th className="textright">Precio Unitario</th>
-                          <th className="textright">Precio Total</th>
-                          <th>Acción</th>
+                          <th id="t_ventas">Código</th>
+                          <th id="t_ventas" colspan="2">Producto</th>
+                          <th id="t_ventas">Cantidad</th>
+                          <th id="t_ventas" className="textright">Precio Unitario</th>
+                          <th id="t_ventas" className="textright">Precio Total</th>
+                          <th id="t_ventas"></th>
                       </tr>
                   </thead>
 
                   <tbody id="detalle_venta">
                       <tr>
                           <td>1001</td>
-                          <td colspan="2">Tennis</td>
-                          <td className="textcenter">1</td>
-                          <td className="textright">100.00</td>
-                          <td className="textright">100.00</td>
-                          <td className=""><a className="link_delate" href="#" onclick= "event.preventDefault(); del_product_detalle(1);"><i className="far fa-trash-alt"></i>Eliminar</a></td>
+                          <td id="t_ventas" colspan="2">Tennis</td>
+                          <td id="t_ventas" className="textcenter">1</td>
+                          <td id="t_ventas" className="textright">100.00</td>
+                          <td id="t_ventas" className="textright">100.00</td>
+                          <td id="t_ventas" className=""><a className="link_delate" href="#" onclick= "event.preventDefault(); del_product_detalle(1);"><i className="far fa-trash-alt"></i>Eliminar</a></td>
                       </tr>
 
                       <tr>
                           <td>1002</td>
-                          <td colspan="2">Camisas</td>
-                          <td className="textcenter">1</td>
-                          <td className="textright">150.00</td>
-                          <td className="textright">100.00</td>
-                          <td className=""><a className="link_delate" href="#" onclick= "event.preventDefault(); del_product_detalle(1);"><i className="far fa-trash-alt"></i></a>
+                          <td id="t_ventas" colspan="2">Camisas</td>
+                          <td id="t_ventas" className="textcenter">1</td>
+                          <td id="t_ventas" className="textright">150.00</td>
+                          <td id="t_ventas" className="textright">100.00</td>
+                          <td id="t_ventas" className=""><a className="link_delate" href="#" onclick= "event.preventDefault(); del_product_detalle(1);"><i className="far fa-trash-alt"></i></a>
                           </td>
                       </tr>
                   </tbody>
                   <tfoot>
                       <tr>
-                          <td colspan="5" className="textright">Total </td>
-                          <td className="textright">1000.00</td>
+                          <td id="t_ventas" colspan="5" className="textright">Total </td>
+                          <td id="t_ventas" className="textright">1000.00</td>
                       </tr>
                   </tfoot>
               </table>
-
             </section>
+            <br/>
+            <br/>
+            <div className="botonesVenta">
+                <a href="#" className="btn_new" id="btn_facturar_venta"><i className="fas fa-edit"></i>Procesar</a>
+                <a href="#" className="btn_ok" id="btn_anular_venta"><i className="fas fa-ban"></i>Anular</a>
+            </div>
         <Footer/>
     </div>
     );
